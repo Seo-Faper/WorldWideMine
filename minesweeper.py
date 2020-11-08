5daf33c546902cafdddac92ede208fd0313b70da
0nederer <Unmistedone@gmail.com>
2020-11-08 23:07:17 +0900

    Update minesweeper.py
    clear condition is not contained

--- minesweeper.py
+++ minesweeper.py
@@ -5,25 +5,26 @@ height = 10
 mine = 18
 
 
-class MineSweeperMaking:
+class MineSweeperMaker:
     def __init__(self):
-        self.mapping = [[0 for i in range(width)] for j in range(height)]
+        self.keep_going = True
+        self.mapping = [[[0] for i in range(width)] for j in range(height)]
         self.plant(self.mapping, mine, 0)
-        self.printMap = ""
         for i in range(height):
             for j in range(width):
-                self.printMap += str(self.mapping[i][j]) + " "
-            self.printMap += "\n"
-        print(self.printMap)
+                self.mapping[i][j].append("X")
+        while self.keep_going:
+            x, y = map(int, input().split())
+            self.clicking(self.mapping, x, y)
 
     def plant(self, temp, mines, numbering):
         while numbering <= mines:
             x = random.randint(0, height-1)
             y = random.randint(0, width-1)
-            if temp[x][y] == "*":
+            if temp[x][y] == ["*"]:
                 return self.plant(temp, mines, numbering)
             else:
-                temp[x][y] = "*"
+                temp[x][y] = ["*"]
                 self.counting(temp, x, y)
                 return self.plant(temp, mines, numbering + 1)
         return temp
@@ -39,9 +40,47 @@ class MineSweeperMaking:
         for i in range(len(stack)):
             a = int(stack[i][0])
             b = int(stack[i][1])
-            if temp[a][b] != "*":
-                temp[a][b] += 1
+            if temp[a][b][0] != "*":
+                temp[a][b][0] += 1
         return temp
 
+    def chain(self, temp, x, y):
+        temp[x][y][1] = temp[x][y][0]
+        stack = []
+        for i in range(3):
+            for j in range(3):
+                stack.append([x + i - 1, y + j - 1])
+        for i in reversed(range(9)):
+            if stack[i][0] < 0 or stack[i][1] < 0 or stack[i][0] > height-1 or stack[i][1] > width-1 or\
+                    (stack[i][0] == x and stack[i][1] == y):
+                del stack[i]
+        for i in range(len(stack)):
+            a = int(stack[i][0])
+            b = int(stack[i][1])
+            if temp[a][b][0] == 0 and temp[a][b][1] == "X":
+                temp[a][b][1] = temp[a][b][0]
+                self.mapping = temp
+                self.chain(temp, a, b)
+            else:
+                temp[a][b][1] = temp[a][b][0]
+                self.mapping = temp
 
-MineSweeperMaking()
+    def printing(self):
+        print_map = ""
+        for i in range(height):
+            for j in range(width):
+                print_map += str(self.mapping[i][j][1]) + " "
+            print_map += "\n"
+        return print_map
+
+    def clicking(self, temp, x, y):
+        if temp[x][y][0] == "*":
+            self.keep_going = False
+            print("Game over")
+        else:
+            self.chain(temp, x, y)
+            print(self.printing())
+
+
+game = MineSweeperMaker
+game()
